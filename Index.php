ab635b89615ff50527e48dbdb7b4873f3de3bcc3
emmanuelsxe <errg07@gmail.com>
2021-01-18 23:27:46 -0600

    Login con Php y MySQL

---
+++ Index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>¡Bienvenido!</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">    
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/partails/style.css">
+</head>
+<body>
+
+    <header>
+        
+    </header>
+
+    <h1>Inicio de Sesión</h1>
+    <a href="login.php">Inicia Sesión</a> 
+    <br>
+    ó
+    <br>
+    <a href="singUp.php">Registrate</a> 
+    
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    
+    <h1>LOGIN</h1>
+    <link rel="preconnect" href="https://fonts.gstatic.com">    
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/partails/style.css">
+    <form action="login.php" method="post">
+        
+        <input type="text" name="email" placeholder="Ingresa tu email">
+        <input type="password" name="password" placeholder="Ingresa tu contraseña">
+        <input type="submit" value="enviar">
+    </form>
+
+</body>
+</html>
